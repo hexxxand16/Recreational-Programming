3581c27eeb8c607813df5b32624ed75aa911e026
hexxxand16 <hexxxand16@gmail.com>
2018-02-06 01:26:47 +1030

    Start game

--- html js/index.js
+++ html js/index.js
@@ -7,18 +7,34 @@ canvas.style.height = "600px";
 var c = canvas.getContext("2d");
 c.scale(2, 2);
 
-c.font = "18px Arial";
-c.lineWidth = 2;
-for (var i = 0; i < 10; i++) {
-    for (var j = 0; j < 10; j++) {
-        c.fillStyle = "rgb(255, 255, 255)";
-        c.rect(80 + 40 * j, 10 + 50 * i, 40, 40);
-        if (j % 2 === 0) {
-            c.fillStyle = "rgb(0, 0, 0)";
-            c.fillRect(82 + 40 * j, 12 + 50 * i, 36, 36);
-        }
+function ship(xpos, ypos) {
+    this.x = xpos;
+    this.y = ypos;
+    this.dx = 1;
+    this.dy = 0;
+
+    this.hp = 50;
+    this.pow = 1;
+
+    this.draw = function() {
+        c.fillStyle = "rgb(0, 0, 0)";
+        c.fillRect(this.x, this.y, 40, 40);
     }
-    c.fillStyle = "rgb(0, 0, 0)";
-    c.fillText("6 6", 20, 38 + 50 * i);
+
+    this.movement = function() {
+        this.x += this.dx;
+        this.y += this.dy;
+    }
 }
-c.stroke();
+
+
+var player = new ship(400, 400);
+
+function update() {
+    requestAnimationFrame(update)
+    c.clearRect(0, 0, innerWidth, innerHeight);
+    player.draw();
+    player.movement();
+}
+
+update()
