e4b381b52edb13d34dfa0609160343480244cc68
hexxxand16 <hexxxand16@gmail.com>
2018-10-06 02:17:19 +0930

    Working on optimisation

--- sevens/sevens.py
+++ sevens/sevens.py
@@ -1,5 +1,4 @@
-from random import shuffle, randrange
-import csv
+from random import shuffle, seed
 
 
 # card object
@@ -7,66 +6,12 @@ class Card:
     def __init__(self, suit, rank, play=0):
         self.suit = suit
         self.rank = rank
-        self.play = play
 
     def __str__(self):
         return "{} of {}".format(self.rank, self.suit)
 
-
-class Game:
-    def __init__(self):
-        deck = createDeck()
-        shuffle(deck)
-        pile = []
-        p1 = deck[0:26]
-        p2 = deck[26:52]
-
-        self.hand = [0] * 52
-        for i in p1:
-            if i.suit is "clubs":
-                self.hand[i.rank - 1] = 1
-            elif i.suit is "spades":
-                self.hand[i.rank + 12] = 1
-            elif i.suit is "diamonds":
-                self.hand[i.rank + 25] = 1
-            else:
-                self.hand[i.rank + 38] = 1
-
-        p1_plays = playableCards(p1, pile)
-        p2_plays = playableCards(p2, pile)
-        for i in p1_plays:
-            if i.suit == "spades":
-                pile.append(i)
-                p1.remove(i)
-                if p2_plays:
-                    num = randrange(0, len(p2_plays))
-                    pile.append(p2_plays[num])
-                    p2.remove(p2_plays[num])
-                    del p2_plays[num]
-
-        for i in p2_plays:
-            if i.suit == "spades" and i.rank == 7:
-                pile.append(i)
-                p2.remove(i)
-
-        while len(p1) > 0 and len(p2) > 0:
-            p1_plays = playableCards(p1, pile)
-            p2_plays = playableCards(p2, pile)
-            if p1_plays:
-                num = randrange(0, len(p1_plays))
-                pile.append(p1_plays[num])
-                p1.remove(p1_plays[num])
-                del p1_plays[num]
-            if p2_plays:
-                num = randrange(0, len(p2_plays))
-                pile.append(p2_plays[num])
-                p2.remove(p2_plays[num])
-                del p2_plays[num]
-
-        if len(p1) == 0:
-            self.hand.append(1)
-        else:
-            self.hand.append(0)
+    def __repr__(self):
+        return "{} of {}".format(self.rank, self.suit)
 
 
 def createDeck():
@@ -78,9 +23,9 @@ def createDeck():
             if j == 7:
                 deck.append(Card(i, j))
             elif j > 7:
-                deck.append(Card(i, j, j - 1))
+                deck.append(Card(i, j))
             elif j < 7:
-                deck.append(Card(i, j, j + 1))
+                deck.append(Card(i, j))
 
     return deck
 
@@ -92,15 +37,124 @@ def playableCards(hand, played):
             playable.append(i)
             continue
         for j in played:
-            if i.suit == j.suit and i.play == j.rank:
-                playable.append(i)
+            if i.suit == j.suit:
+                if i.rank > 7 and j.rank == i.rank - 1:
+                    playable.append(i)
+                elif i.rank < 7 and j.rank == i.rank + 1:
+                    playable.append(i)
 
     return playable
 
 
-with open("sevens.csv", "w", newline='') as f:
-    for i in range(100000):
-        game = Game()
-        a = game.hand
-        writer = csv.writer(f)
-        writer.writerows([a])
+def play(p1, p2, pile, first, depth):
+    p1_plays = playableCards(p1, pile)
+    p2_plays = playableCards(p2, pile)
+    # Heuristic
+    if depth == 0:
+        return len(p1_plays) - len(p2_plays)
+    if not p1:
+        return 10000
+    elif not p2:
+        return -10000
+
+    if first:
+        score = -10000
+        if not p1_plays:
+            return play(p1, p2, pile, False, depth - 1)
+        for i in p1_plays:
+            new_pile = pile.copy()
+            new_p1 = p1.copy()
+            new_pile.append(i)
+            new_p1.remove(i)
+            score = play(new_p1, p2, new_pile, False, depth - 1)
+    else:
+        score = 10000
+        if not p2_plays:
+            return play(p1, p2, pile, True, depth - 1)
+        for i in p2_plays:
+            new_pile = pile.copy()
+            new_p2 = p2.copy()
+            new_pile.append(i)
+            new_p2.remove(i)
+            score = play(p1, new_p2, new_pile, True, depth - 1)
+
+    return score
+
+
+def evaluate(p1, p2, pile, first, depth):
+    if first:
+        p1_plays = playableCards(p1, pile)
+        print("P1: " + str(p1_plays))
+        score = -20000
+        best_move = 0
+        for i in p1_plays:
+            new_pile = pile.copy()
+            new_p1 = p1.copy()
+            new_pile.append(i)
+            new_p1.remove(i)
+            cur_score = max(score, play(new_p1, p2, new_pile, False, depth))
+            if cur_score > score:
+                score = cur_score
+                best_move = i
+            print(str(i) + ": " + str(cur_score))
+
+        print(str(best_move) + ": index " + str(p1.index(best_move)))
+    else:
+        p2_plays = playableCards(p2, pile)
+        print("P2: " + str(p2_plays))
+        score = 20000
+        best_move = 0
+        for i in p2_plays:
+            new_pile = pile.copy()
+            new_p2 = p2.copy()
+            new_pile.append(i)
+            new_p2.remove(i)
+            cur_score = min(score, play(p1, new_p2, new_pile, True, depth))
+            if cur_score < score:
+                score = cur_score
+                best_move = i
+            print(str(i) + ": " + str(cur_score))
+
+        print(str(best_move) + ": index " + str(p2.index(best_move)))
+    return best_move
+
+
+def playCard(p1, p2, card, pile):
+    if card in p1:
+        pile.append(card)
+        p1.remove(card)
+    elif card in p2:
+        pile.append(card)
+        p2.remove(card)
+
+
+def main():
+    # Seed 30: P1 gets 4 7s
+    # Seed 42: Fair game
+    seed(42)
+    deck = createDeck()
+    shuffle(deck)
+    p1 = deck[0:26]
+    p2 = deck[26:52]
+    pile = []
+    
+    playCard(p1, p2, p2[7], pile)
+    first = True
+    while p1 or p2:
+        if first:
+            if not playableCards(p1, pile):
+                first = False
+                continue
+            move = evaluate(p1, p2, pile, True, 8)
+            playCard(p1, p2, move, pile)
+            first = False
+        else:
+            if not playableCards(p2, pile):
+                first = True
+                continue
+            move = evaluate(p1, p2, pile, False, 8)
+            playCard(p1, p2, move, pile)
+            first = True
+
+
+main()
